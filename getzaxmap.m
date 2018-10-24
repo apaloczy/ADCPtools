ee3a19dbf33baed366d1c8fa94ac6c0e8c324a7b
André Palóczy <paloczy@gmail.com>
2018-10-24 11:34:46 -0700

    Add getzaxmap helper function.

---
+++ getzaxmap.m
@@ -0,0 +1,16 @@
+function Zmap = getzaxmap(rbeam, theta)
+% USAGE
+% -----
+% mapped_zaxis = getzaxmap(rbeam, theta)
+%
+% Returns the projection of the along-beam coordinate 'rbeam'
+% from the local vertical axis. The along-beam direction is tilted
+% 'theta' degrees from the vertical.
+  Zmap = rbeam.*cos(theta.*pi/180);
+  dZu = Zmap(end) - Zmap(end-1);
+  rmax = rbeam(end);
+  nz = length(Zmap);
+  while Zmap(end)<(rmax-dZu)
+    Zmap = [Zmap Zmap(end)+dZu]; % Complete vertical axis to map to.
+  end
+end
