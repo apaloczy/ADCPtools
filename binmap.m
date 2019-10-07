49dcea72135e0c92118db9b5a699f5602818ada3
André Palóczy <paloczy@gmail.com>
2019-10-06 21:43:40 -0700

    Fix handling of bins outside range.

--- binmap.m
+++ binmap.m
@@ -18,8 +18,6 @@ Cph3 = cos(roll);
 Z = r.*Cth;
 Zbot = Z(1);
 Ztop = Z(end);
-dZu = Z(end) - Z(end-1);
-rmax = r(end);
 z00 = [0 0 1]';
 
 [nz, nt] = size(b1);
@@ -39,38 +37,38 @@ Bo = cat(3, b1, b2, b3, b4);
 
 for i=1:4
   Ei = E(:,i);
+
   Boi = Bo(:,:,i); % z, t, bi.
+  bmi = Boi;
 
   for k=1:nt
     zi = abs((PR(:,:,k)*Ei)'*z00).*r; % Actual bin height, dot product of tilt matrix with along-beam distance vector.
-    boi = Boi(:,k);
-    dboidzbot = (boi(1) - boi(2))./(zi(1) - zi(2));
-    dboidztop = (boi(end) - boi(end-1))./(zi(end) - zi(end-1));
 
-    for J=1:nz
+    % Check whether bins are lower than bottom or higher than top.
+    nbot = 1;
+    ntop = nz;
+    zlo = zi<Zbot;
+    zhi = zi>Ztop;
+    if any(zlo)
+      ntop = nz - sum(zlo);
+    end
+    if any(zhi)
+      nbot = sum(zhi);
+    end
+
+    boi = Boi(:,k);
+
+    for J=nbot:ntop
       Zj = Z(J);
       if strcmp(how, 'linear')                         % Linear interpolation.
         j = nearfl(zi, Zj);
         jj = j + 1;
-        if jj>nz
-          jj = nz;
-          j = nz - 1;
-        end
         zij = zi(j);
         zijj = zi(jj);
-        if Zbot>zij
-          bmi(J,k) = boi(1) + (zij - Zbot).*dboidzbot;
-        elseif Ztop<zijj
-          bmi(J,k) = boi(end) + (zijj - Ztop).*dboidztop;
-        else
-          dzj = zijj - zij;
-          bmi(J,k) = ((Zj - zij)./dzj).*boi(j) + ((zijj - Zj)./dzj).*boi(jj);
-        end
+        dzj = zijj - zij;
+        bmi(J,k) = ((Zj - zij)./dzj).*boi(j) + ((zijj - Zj)./dzj).*boi(jj);
       elseif strcmp(how, 'nn')                         % Nearest-neighbor interpolation.
         j = near(zi, Zj);
-        if j>nz
-          j = nz;
-        end
         bmi(J,k) = boi(j);
       end
     end
